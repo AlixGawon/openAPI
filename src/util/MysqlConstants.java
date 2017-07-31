8892abf9b6aaeb9109539484a937ccb885059dd0
alix <dlrkdnjs1211@gmail.com>
2017-07-31 10:36:23 +0900

    initial

---
+++ src/util/MysqlConstants.java
@@ -0,0 +1,18 @@
+package util;
+
+/**
+ * Created by alix on 2017-07-16.
+ */
+public interface MysqlConstants {
+
+
+        String DRIVER_MYSQL = "org.postgresql.Driver";
+
+        String DB_URL = "jdbc:postgresql://localhost:5432/northwind";
+
+        String USERNAME = "";
+
+        String PASSWORD = "kitri";
+
+
+}
